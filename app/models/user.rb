f7e73b937f5516aeb0e7734ecf5d5b574eb34c3a
EC2 Default User <ec2-user@ip-172-31-3-94.ap-northeast-1.compute.internal>
2023-01-28 02:37:50 +0000

    [Add]フォロー機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,12 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :followings, through: :relationships, source: :followed
+  has_many :follower, through: :reverse_of_relationships, source: :follower
+
   validates :name, presence: true
 
 
@@ -18,6 +24,18 @@ class User < ApplicationRecord
       profile_image.attach(io:File.open(file_path),filename: 'default-image.jpeg',content_type: 'image/jpeg')
     end
       profile_image.variant(resize_to_limit:[width,height]).processed
+  end
+
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'about'=>'homes#about',as: 'about'
-  resources :users, only:[:index,:show,:edit,:update]
+  resources :users, only:[:index,:show,:edit,:update] do
+    resource :relationships, only:[:create,:destroy]
+    get 'followings'=>'relationships#followings', as: 'followings'
+    get 'followers'=>'relationships#followers', as: 'followers'
+  end
+  
   resources :books, only:[:index,:show,:edit,:create,:update,:destroy] do
     resource :favorites, only:[:create,:destroy]
     resources :book_comments, only:[:create,:destroy]
